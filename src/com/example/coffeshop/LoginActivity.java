6ea47eaa7bad4e85ae8bb0cd5ab07919a0735ff3
stkorn <s.sommechai@gmail.com>
2015-03-27 17:08:34 +0700

    add layout main page

--- src/com/example/coffeshop/LoginActivity.java
+++ src/com/example/coffeshop/LoginActivity.java
@@ -2,14 +2,20 @@ package com.example.coffeshop;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class LoginActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
+    LinearLayout bar1;
+    LinearLayout bar2;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        bar1 = (LinearLayout) findViewById(R.id.bar1);
+        bar2 = (LinearLayout) findViewById(R.id.bar2);
+
     }
 }
